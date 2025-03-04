561a29764342863e99035884608ec2f26f5de17d
kleemann <kleemann@cip.ifi.lmu.de>
2025-03-04 04:29:18 +0100

    added train test split

--- pdll/_pairwise_NLP.py
+++ pdll/_pairwise_NLP.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 import pandas as pd
+
+SEED = 17
 
 
 # Input data file from directory and preprocessing
@@ -28,8 +31,23 @@ for key in dictOfEssaySets.keys():
 # establish a simple reduced dataset of only the first essay set for development
 prelimReducedData = dictOfEssaySets[essayPromptSetID[0]]
 
+prelimReducedData = prelimReducedData.dropna(axis=1, how="all")
+
+# print(prelimReducedData.essay_id.unique().nonzero())
+
 
 # train test split
+
+prelimReducedData = prelimReducedData.sample(frac=1, random_state=SEED)
+
+lablesTrue = prelimReducedData.iloc[:, 3:]
+data_stripped = prelimReducedData.iloc[:, :3]
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    data_stripped, lablesTrue, test_size=0.25, random_state=SEED, shuffle=True
+)
+
+print(X_train.head(9))
 
 
 # d2 = data.head(9)[data.columns[:3]]
