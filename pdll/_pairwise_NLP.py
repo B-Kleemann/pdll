840ee92adba09ac68f44ecb03325e5548132031c
kleemann <kleemann@cip.ifi.lmu.de>
2025-05-09 01:48:31 +0200

    altered baseline predictor to mimic scoring by two experts

--- pdll/_pairwise_NLP.py
+++ pdll/_pairwise_NLP.py
@@ -61,20 +61,20 @@ def get_pair_diff_as_int(text1: str, text2: str, rubric: str) -> int:
         raise RuntimeError(f"OpenAI API call failed: {api_err}")
 
 
-{
-    # diff_1 = get_pair_diff_as_int(
-    #     X_train.iloc[0].essay, X_train.iloc[1].essay, rubric_set_1_text
-    # )
-    # diff_2 = get_pair_diff_as_int(
-    #     X_train.iloc[0].essay, X_train.iloc[1].essay, rubric_set_1_text
-    # )
-    # if diff_1 != diff_2:
-    #     print(f"Different results! {diff_1} != {diff_2}")
-    # else:
-    #     print(f"Same results! Both say {diff_1}")
-    # ? variation of the differences is high, so the model is not consistent in its predictions
-    # ? range tested with this essay pair is -1 to 3, -1 x 1, 0 x 3, 1 x 2, 2 x 3, 3 x 1
-}  # type: ignore
+# {
+#     # diff_1 = get_pair_diff_as_int(
+#     #     X_train.iloc[0].essay, X_train.iloc[1].essay, rubric_set_1_text
+#     # )
+#     # diff_2 = get_pair_diff_as_int(
+#     #     X_train.iloc[0].essay, X_train.iloc[1].essay, rubric_set_1_text
+#     # )
+#     # if diff_1 != diff_2:
+#     #     print(f"Different results! {diff_1} != {diff_2}")
+#     # else:
+#     #     print(f"Same results! Both say {diff_1}")
+#     # ? variation of the differences is high, so the model is not consistent in its predictions
+#     # ? range tested with this essay pair is -1 to 3, -1 x 1, 0 x 3, 1 x 2, 2 x 3, 3 x 1
+# }  # type: ignore
 
 
 def predict_scores_pairwise(
@@ -155,8 +155,11 @@ def predict_scores_solo(test_data: pd.DataFrame, rubric: str) -> pd.DataFrame:
     predictions = []
     for i, row_i in test_data.iterrows():
         try:
-            # the score here is doubled, because the original score is the sum of two single scores by two different experts
-            score_pred = 2 * get_essay_score_as_int(row_i["essay"], rubric)
+            # the score here is predicted twice to mimic the composition of the original score (the sum of two single scores by two different experts)
+            score_pred_1 = get_essay_score_as_int(row_i["essay"], rubric)
+            score_pred_2 = get_essay_score_as_int(row_i["essay"], rubric)
+
+            score_pred = score_pred_1 + score_pred_2
             predictions.append(score_pred)
 
         except RuntimeError:
@@ -173,7 +176,7 @@ data_train, data_dev, data_test = data_processing.get_data(
     FOLD_ID, essay_set_ID, as_list_of_tuples
 )
 
-limit = 6  # Limit the number of rows for testing
+limit = 4  # Limit the number of rows for testing
 limit_data = limit  # Limit the number of rows for testing
 limit_baseline = limit  # Limit the number of rows for testing
 
@@ -190,12 +193,12 @@ data_train, data_dev, data_test = (
 
 #! limits data for development and testing
 data_train, data_dev, data_test = (
-    # data_train.tail(limit_baseline),
-    # data_dev.tail(limit_data),
-    # data_test.tail(limit),
-    data_train.head(limit_baseline),
-    data_dev.head(limit_data),
-    data_test.head(limit),
+    data_train.tail(limit_baseline),
+    data_dev.tail(limit_data),
+    data_test.tail(limit),
+    # data_train.head(limit_baseline),
+    # data_dev.head(limit_data),
+    # data_test.head(limit),
 )
 
 # pairwise_score_prediction = predict_scores_pairwise(
