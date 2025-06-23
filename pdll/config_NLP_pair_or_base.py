082c96dc09b2d6b7d2551142ce2ed0c5b9df14a9
B-Kleemann <bettina.bk.kleemann@gmail.com>
2025-06-23 05:29:25 +0200

    added pairwise runs; exclusion of o3mini due to intensity

--- pdll/config_NLP_pair_or_base.py
+++ pdll/config_NLP_pair_or_base.py
@@ -3,11 +3,10 @@ llms = [
     "gpt-4o-mini",
     "gpt-4.1-mini",
     "gpt-4.1-nano",
-    "o3-mini",
+    # "o3-mini",
     "o4-mini",
     "gpt-4o",
     "gpt-4.1",
-    "o3",
 ]
 
 # Set control variables
@@ -15,19 +14,19 @@ is_test_run = True
 
 is_pairwise = False
 
-llm = llms[1]
+llm = llms[5]
 
-start_at_essay_set = 7
-stop_at_essay_set = 1 + 7
+start_at_essay_set = 1
+stop_at_essay_set = 1 + 8
 
 fold_ID = 1
 random_seed = 81
 
 if is_pairwise:
-    limit_data = 50
+    limit_data = 30
     limit_anchors = 10
     limit_reasonable = 60
 else:
-    limit_data = 500
+    limit_data = 30
     limit_anchors = 0
-    limit_reasonable = 600
+    limit_reasonable = 60
